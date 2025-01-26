1e4b22462f1eb5161e915a80df14a54f669cb31b
baskar142 <baskarpmkevin@gmail.com>
2025-01-26 18:21:30 +0530

    utils added

--- demo.py
+++ demo.py
@@ -1,3 +1,10 @@
 from us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys
 
-logging.info("Welcome to custom log")
+#logging.info("Welcome to custom log4")
+
+try:
+    a=20/0
+except Exception as e:
+    raise   USvisaException(e,sys)
--- us_visa/logger/__init__.py
+++ us_visa/logger/__init__.py
@@ -1,18 +1,17 @@
 import logging
 import os
-
-from from_root import from_root
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-
 log_dir = 'logs'
 
-logs_path = os.path.join(from_root(), log_dir, LOG_FILE)
+# Ensure path is correct relative to root or desired directory
+logs_path = os.path.join(os.getcwd(), log_dir, LOG_FILE)
 
-os.makedirs(log_dir, exist_ok=True)
+# Create the log directory if it doesn't exist
+os.makedirs(os.path.join(os.getcwd(), log_dir), exist_ok=True)
 
-
+# Basic logging configuration
 logging.basicConfig(
     filename=logs_path,
     format="[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
