aca031789c4233f724bd00ac5da6506a92a836b9
baskar142 <baskarpmkevin@gmail.com>
2025-01-26 17:23:20 +0530

    mangodbdemo

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Baskar",
+    author_email="baskaranpandiyan14@gmail.com",
+    packages=find_packages()
+)
